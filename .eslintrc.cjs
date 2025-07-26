f5b683cbd499395cfee1b61b7876addc25b3d8b8
yavarguliyev <yavarrg@code.edu.az>
2025-07-26 13:40:52 +0400

    chore: initial project setup, base dependencies added

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+};
